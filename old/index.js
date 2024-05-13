7dfaae1b6524a29688f630ac995fa6d52d538df6
Alyson F Souza <alysonforever@gmail.com>
2024-05-13 14:03:43 -0300

    refactor: restart project with Next.js, move old files to "old" directory
    The project is being restarted with Next.js. All the old files have been moved to the "old" directory and compressed. This is the start of a new version. The project is now a monorepo using yarn workspace.
    
    BREAKING CHANGE: The page will no longer be loaded.

--- old/index.js
+++
@@ -1,6 +0,0 @@
-const currentYearEl = document.querySelector("span#current-year");
-
-currentYearEl.innerHTML = new Date().getUTCFullYear();
-
-const skillsTag = document.querySelector("section#skills");
-const skillsFront = ["HTML", "CSS", "JavaScript", "React", "React Native"];
